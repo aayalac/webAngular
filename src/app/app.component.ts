7079bd8e785a4a48e8a86786a1a2d4c6f48532ce
aayalac <aayalac@misena.edu.co>
2022-10-19 10:15:25 -0500

    Cambio de Root a Raiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'webAngular';
+  saludo = "Esta es mi primera interaccion con angular, super!!!!!"
 }
